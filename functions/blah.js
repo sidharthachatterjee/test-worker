b66ecd18cf32ba168c0175f33398abd1c5d7c384
Sid Chatterjee <sid@cloudflare.com>
2022-10-27 22:46:43 +0100

    Three blahs

--- functions/blah.js
+++ functions/blah.js
@@ -1,4 +1,3 @@
 export async function onRequest() {
-  await fetch("https://solitary-star-c9fa.sid7002.workers.dev/");
-  return new Response("blah blah blah blah");
+  return new Response("blah blah blah");
 }
