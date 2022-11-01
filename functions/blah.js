9a94d08ab71287b401ea9b6025d6f17ee4f371f6
Sid Chatterjee <sid@cloudflare.com>
2022-11-01 19:53:24 +0000

    try again

--- functions/blah.js
+++ functions/blah.js
@@ -1,8 +1,7 @@
 export async function onRequest(context) {
   const { request, env } = context;
-  const url = new URL(request.url, {
-    pathname: `foo`,
-  });
+  const url = new URL(request.url);
+  url.pathname = `foo`;
   console.log(url);
   const req = new Request(url);
   return env.ASSETS.fetch(req);
